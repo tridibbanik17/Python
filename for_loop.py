190ee3493463934e9530ac29860f13c26eed136e
Tridib Banik <tridib.perfect@gmail.com>
2025-07-27 01:13:59 -0400

    Basic for loop in Python

---
+++ for_loop.py
@@ -0,0 +1,12 @@
+#This program calculate the sum of 0 through 9 (including 9).
+
+#Initialize sum. 
+sum = 0
+
+for i in range(10): #iterate from 0 through 9.
+    sum = sum + i #formula
+    print(sum) #print updated sum each time (total 10 times)
+
+
+    
+    
