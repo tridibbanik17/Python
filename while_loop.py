625b1a37547e3ab9e63bba7efb8c367874259767
Tridib Banik <tridib.perfect@gmail.com>
2025-07-28 18:10:50 -0400

    Basic While Loop in Python

---
+++ while_loop.py
@@ -0,0 +1,13 @@
+#This program calculate the sum of 0 through 9 (including 9).
+
+#Initialize sum.
+sum = 0
+
+#Initialize i.
+i = 0
+
+while i < 10: #Using while loop and writing the condition of when the program stops.
+    sum = sum + i #Writing the formula.
+    print(sum) #print the updated sum.
+    i = i + 1 #Indicating how the value of i will be increasing.
+
