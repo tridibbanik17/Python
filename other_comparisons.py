c89cb249ee97d6aa7edfc9aa3680d6b6c78669c9
Tridib Banik <tridib.perfect@gmail.com>
2026-05-08 00:15:39 -0400

    Empty string is falsy

--- other_comparisons.py
+++ other_comparisons.py
@@ -35,3 +35,28 @@ print(f"'time' < 'money' is {'time' < 'money'}")               # False
 print(f"'time' == 'turn' is {'time' == 'turn'}")               # False
 print(f"'time' >= 'turn' is {'time' >= 'turn'}")               # False
 print(f"'time' <= 'turn' is {'time' <= 'turn'}")               # True
+
+print(f"Boolean of empty string is {bool('')}" )               # False (empty string is falsy) 
+print(f"Boolean of non-empty string is {bool(' ')}")           # True (non-empty string is truthy)
+
+print(f"Boolean of 0 is {bool(0)}")                           # False (0 is falsy)
+print(f"Boolean of non-zero number is {bool(0.1)}")           # True (non-zero number is truthy)
+
+print(f"Boolean of None is {bool(None)}")                     # False (None is falsy)
+print(f"Boolean of empty list is {bool([])}")                 # False (empty list is falsy)
+print(f"Boolean of empty dict is {bool({})}")                 # False (empty dictionary is falsy)
+
+print(f"Boolean of empty tuple is {bool(())}")                # False (empty tuple is falsy)
+print(f"Boolean of tuple with one element is {bool((1,))}")   # True (tuple with one element is truthy)
+
+print(f"Boolean of empty set is {bool(set())}")               # False (empty set is falsy)
+print(f"Boolean of set with one element is {bool({1})}")      # True (set with one element is truthy)
+
+print(f"Boolean of empty range is {bool(range(0))}")          # False (empty range is falsy)
+print(f"Boolean of range with 1 element is {bool(range(1))}") # True (range with one element is truthy)
+
+print(f"0 == False is {0 == False}")                          # True (0 is equal to False)
+print(f"0 is False is {0 is False}")                          # False (0 and False are not the same object)
+
+print(f"0 == None is {0 == None}")                           # False (0 is not equal to None)
+print(f"0 is None is {0 is None}")                           # False (0 and None are not the same object)
