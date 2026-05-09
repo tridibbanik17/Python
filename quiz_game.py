855514338e8a11eac1a8c169f05d2ce926c62d86
Tridib Banik <tridib.perfect@gmail.com>
2026-05-08 21:19:53 -0400

    Quiz game where user guesses a list of countries starting with 'V'

---
+++ quiz_game.py
@@ -0,0 +1,44 @@
+# Create a list of answers (e.g., countries that start with 'V')
+# The user tries to get all of the answers
+# If they give up and quit, print out all of the ones that were missed
+
+def play():
+    answer_list = ["Vatican City", "Vanuatu", "Venezuela", "Vietnam"]
+    num_of_answers = len(answer_list)
+    user_answer_list = []
+    missed_answers = []
+    print("""
+          Game rules:
+          - You have to guess the countries that start with 'V'.
+          - You can guess in any order.
+          - You can only guess one country at a time.
+          - You can quit at any time by typing 'Quit'.
+          """)
+    while len(user_answer_list) != len(answer_list):
+        if num_of_answers == 1:
+            suffix = ''
+        else:
+            suffix = 's'
+        print(f"{num_of_answers} correct answer{suffix} remaining.")
+        user_answer = input("Enter the country: ").strip().title()
+        if user_answer in user_answer_list:
+            print("Already guessed. Try again.")
+        elif user_answer in answer_list:
+            user_answer_list.append(user_answer)
+            print("Correct!")
+            num_of_answers = num_of_answers - 1
+        elif user_answer == 'Quit':
+            for answer in answer_list:
+                if answer not in user_answer_list:
+                    missed_answers.append(answer)
+            break
+        else:
+            print("Incorrect. Try again.")
+    if len(missed_answers) == 0:
+        print("Congratulations! You got all the answers!")
+    else: 
+        print(f"You missed the following {len(missed_answers)} answers:")
+        missed = ', '.join(missed_answers)
+        print(missed)
+
+play()
